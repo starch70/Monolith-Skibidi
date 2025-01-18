e3b096faa9d38090f234344d0e6a112af3ff7196
Ark <189933909+ark1368@users.noreply.github.com>
2025-01-18 00:05:58 -0700

    CDN Forkify (#6)

--- Tools/publish_multi_request.py
+++ Tools/publish_multi_request.py
@@ -14,8 +14,8 @@ RELEASE_DIR = "release"
 # CONFIGURATION PARAMETERS
 # Forks should change these to publish to their own infrastructure.
 #
-ROBUST_CDN_URL = "https://cdn.simplestation.org/"
-FORK_ID = "goobstation-lrp"
+ROBUST_CDN_URL = "https://cdn.project-monolith.xyz/"
+FORK_ID = "monolith"
 
 def main():
     session = requests.Session()
