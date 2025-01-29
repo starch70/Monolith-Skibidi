ab1db7796609b79e1005b67fb7781a7b7aec05bb
Ark <189933909+ark1368@users.noreply.github.com>
2025-01-28 18:48:42 -0700

    Autovote & Respawn Hotfix? (#28)
    * AUTOMATE ALL!
    
    * Respawn Hotfix

--- Content.Shared/CCVar/CCVars.GhostRespawn.cs
+++ Content.Shared/CCVar/CCVars.GhostRespawn.cs
@@ -4,8 +4,8 @@ namespace Content.Shared.CCVar;
 
 public sealed partial class CCVars
 {
-    public static readonly CVarDef<int> GhostRespawnTime =
-        CVarDef.Create("ghost.respawn_time", 15, CVar.SERVERONLY);
+    public static readonly CVarDef<double> GhostRespawnTime =
+        CVarDef.Create("ghost.respawn_time", 15d, CVar.SERVERONLY);
 
     public static readonly CVarDef<int> GhostRespawnMaxPlayers =
         CVarDef.Create("ghost.respawn_max_players", 40, CVar.SERVERONLY);
